a7cd6c99a99b8645430054d368db054fc961193c
Willow Fox Fortino <fortino@udel.edu>
2023-04-05 12:32:35 -0400

    Updaing training script with up to date syntax

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 
 # SBATCH --gpus=tesla_t4:1
@@ -22,13 +22,11 @@
 #SBATCH --requeue
 #SBATCH --export=ALL
 
-#SBATCH --array=0-32
+#SBATCH --array=0-200
 
 UD_QUIET_JOB_SETUP=YES
-. /opt/shared/slurm/templates/libexec/common.sh
 
 echo "SLURM ARRAY TASK ID: $SLURM_ARRAY_TASK_ID"
 echo "SLURM RESTART COUNT: $SLURM_RESTART_COUNT"
 
-conda activate fox
-python /home/2649/repos/SCS/scs/execute_train.py --R=100 --array_index=$SLURM_ARRAY_TASK_ID --num_requeue=$SLURM_RESTART_COUNT --model_search_name="aug2"
+python /home/2649/repos/SCS/scs/learn.py --hp_set="dev9" --R=100 --array_index=$SLURM_ARRAY_TASK_ID --num_requeue=$SLURM_RESTART_COUNT
